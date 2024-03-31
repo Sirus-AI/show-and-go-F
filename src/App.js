35d75e451aa195386f9419eedc17f6b9e55d3768
ALISHACODER30 <alishaaliataneja@email.com>
2024-03-31 15:36:45 +0530

    restored index

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Report from './Component/attendancere'
 
 const App = () => {
   return (
     <div>
-      
+      <Report/>
     </div>
   )
 }
---
+++ src/Component/Attendancere.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Report = () => {
+    return (
+    <h2> hey this is report component ! </h2>
+    )
+ } 
+
+ export default Report
