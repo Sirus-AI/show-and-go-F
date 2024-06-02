cc05cf4281c07d55ad6342118e6bfb9e4ef1ee3e
Shuru63 <shubhamkuargarg63@gmail.com>
2024-06-03 03:01:10 +0530

    add organization modal

--- src/Screen/Admin_dashboard/Admindashboard.jsx
+++ src/Screen/Admin_dashboard/Admindashboard.jsx
@@ -1,61 +1,223 @@
 import React from 'react'
+import { useState, useCallback, useEffect } from 'react'
 import Navbar from '../../Component/Navigation/Navbar'
-import { useState } from 'react'
+import server from '../../Server'
+import { CModal, CModalBody, CModalFooter, CModalHeader, CModalTitle, CButton } from '@coreui/react';
 import Admin from '../../static_content/admin_content/Screenshot.png'
 const Admindashboard = () => {
     const [isNavbarOpen, setIsNavbarOpen] = useState(false);
-
+    const [message, setMessage] = useState('');
+    const [visible, setVisible] = useState(false);
+    const [formVisible, setFormVisible] = useState(false);
+    const [name, setName] = useState('');
+    const [about, setAbout] = useState('');
+    const [location, setLocation] = useState('');
+    const [added_date, setAddedDate] = useState('');
+    const [organisations, setOrganisations] = useState([]);
+    const [users, setUsers] = useState([]);
+console.log(users)
     const toggleSidebar = () => {
         setIsNavbarOpen(!isNavbarOpen);
     };
+    const btnModal = () => {
+        setVisible(true)
+        setFormVisible(false)
+    }
+    const formModal = () => {
+        setFormVisible(true)
+        setVisible(false)
+    }
+    const fetchUserdata= useCallback(async () => {
+        server
+            .get(`api/users/user/profile/`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': '{{ csrf_token }}',
+                },
+            })
+            .then((response) => {
+                setUsers(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    })
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!name || !about || !location || !added_date || !organisations) {
+            setMessage('All fields are required');
+        } else {
+            setMessage('');
+            console.log('Form submitted', { name, about, location, added_date, organisations });
+            setFormVisible(false);
+        }
+    };
 
+    useEffect(() => {
+        btnModal()
+        fetchUserdata();
+    }, []);
     return (
-        <div className='home-page'>
-            <Navbar toggleSidebar={toggleSidebar} />
-            <div className='dashboard-content'>
-                <div className={isNavbarOpen ? 'content-cover' : 'content-toggle'}>
-                    <p className='overview'>Overview</p>
-                    <div className='dashboard-card'>
+        <div>
+            <CModal
+                visible={visible}
+                onClose={() => setVisible(false)}
+                aria-labelledby="LiveDemoExampleLabel"
+                alignment="center"
+            >
+                <CModalHeader onClose={() => setVisible(false)}>
+                    <CModalTitle id="LiveDemoExampleLabel" >Harry up</CModalTitle>
+                </CModalHeader>
+                <CModalBody>
+                    <div className='modalbody'>
+                        <p className='modal-para'>you are not register in company please register now</p>
+                        <button className='modal-btn' onClick={() => formModal()}>Register</button>
+                    </div>
+                </CModalBody>
+                <CModalFooter>
+                    <CButton color="secondary" onClick={() => setVisible(false)}>
+                        Close
+                    </CButton>
+                </CModalFooter>
+            </CModal>
+            <CModal
+                visible={formVisible}
+                onClose={() => setFormVisible(false)}
+                aria-labelledby="LiveDemoExampleLabel"
+                alignment="center"
+            >
+                <CModalHeader onClose={() => setFormVisible(false)}>
+                    <CModalTitle id="LiveDemoExampleLabel" >Harry up</CModalTitle>
+                </CModalHeader>
+                <CModalBody>
+                    <div className="register-display">
+                        <div className='modal-form'>
+                            <h3 className='h1'>ORGANISATION REGISTRATION</h3>
+                            <form className='form' onClick={handleSubmit}>
+                                <div className='credential'>
+                                   
+                                        <div className='credential'>
+                                            <div className='flex'>
+                                                <label htmlFor="fname">Name</label>
+                                            </div>
+                                            <input
+                                                type="text"
+                                                name="fname"
+                                                id="fname"
+                                                placeholder="Name"
+                                                className='email'
+                                                onChange={(e) => setName(e.target.value.trim())}
+                                            />
+                                        </div>
+                                       
+                                   <div className='credential'>
+                                            <div className='flex'>
+                                                <label htmlFor="lname">About</label>
+                                            </div>
+                                            <input
+                                                type="text"
+                                                name="lname"
+                                                id="lname"
+                                                placeholder="About"
+                                                className='email'
+                                                onChange={(e) => setAbout(e.target.value.trim())}
+                                            />
+                                        </div>
+                                </div>
+                                <div className='credential'>
+                                    <div className='flex'>
+                                        <label htmlFor="email">Location</label>
+                                    </div>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        placeholder="location"
+                                        className='email'
+                                        onChange={(e) => setLocation(e.target.value.trim())}
+                                    />
+                                </div>
+                                <div className='credential'>
+                                    <div className='flex'>
+                                        <label htmlFor="phone">Added Date</label>
+                                    </div>
+                                    <input
+                                        type="date"
+                                        name="dob"
+                                        placeholder="date*"
+                                        className='email'
+                                        onChange={(e) => setAddedDate(e.target.value.trim())}
+                                    />
+                                </div>
+                                <div className='credential'>
+                                    <div className='flex'>
+                                        <label htmlFor="phone">Organisations</label>
+                                    </div>
+                                    <input
+                                        type="tel"
+                                        name="phone"
+                                        placeholder="Organisations"
+                                        className='email'
+                                        onChange={(e) => setOrganisations(e.target.value.trim())}
+                                    />
+                                </div>
+                                <button className='regis' type="submit">Register</button>
+                            </form>
+                        </div>
+                    </div>
+                </CModalBody>
+                <CModalFooter>
+                    <CButton color="secondary" onClick={() => setFormVisible(false)}>
+                        Close
+                    </CButton>
+                </CModalFooter>
+            </CModal>
+            <div className='home-page'>
+                <Navbar toggleSidebar={toggleSidebar} />
+                <div className='dashboard-content'>
+                    <div className={isNavbarOpen ? 'content-cover' : 'content-toggle'}>
+                        <p className='overview'>Overview</p>
+                        <div className='dashboard-card'>
 
-                        <div className='card'>
-                            <span class="material-symbols-outlined card-span">
-                                person
-                            </span>
-                            <div className='present'>
-                                <p>Total</p>
+                            <div className='card'>
+                                <span class="material-symbols-outlined card-span">
+                                    person
+                                </span>
+                                <div className='present'>
+                                    <p>Total</p>
 
+                                </div>
                             </div>
-                        </div>
-                        <div className='card'>
-                            <span class="material-symbols-outlined card-span">
-                                diversity_3
-                            </span>
-                            <div className='present'>
-                                <p>Present</p>
-                                <p>375 <span>65%</span></p>
+                            <div className='card'>
+                                <span class="material-symbols-outlined card-span">
+                                    diversity_3
+                                </span>
+                                <div className='present'>
+                                    <p>Present</p>
+                                    <p>375 <span>65%</span></p>
+                                </div>
                             </div>
-                        </div>
-                        <div className='card'>
-                            <span class="material-symbols-outlined card-span">
-                                person_remove
-                            </span>
-                            <div className='present'>
-                                <p>Absent</p>
-                                <p>375 <span>65%</span></p>
+                            <div className='card'>
+                                <span class="material-symbols-outlined card-span">
+                                    person_remove
+                                </span>
+                                <div className='present'>
+                                    <p>Absent</p>
+                                    <p>375 <span>65%</span></p>
+                                </div>
                             </div>
-                        </div>
-                        <div className='card'>
-                            <span class="material-symbols-outlined card-span">
-                                person_add
-                            </span>
-                            <div className='present'>
-                                <p>Total</p>
-                                <p>375 <span>65%</span></p>
+                            <div className='card'>
+                                <span class="material-symbols-outlined card-span">
+                                    person_add
+                                </span>
+                                <div className='present'>
+                                    <p>Total</p>
+                                    <p>375 <span>65%</span></p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div></div>
+                </div></div></div>
     )
 }
 
