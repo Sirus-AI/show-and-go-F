3eb9a669ab0e191fb7510eb63210d4c7fb6238a5
Nehamalwal <nehaverma5017@gmail.in>
2024-06-06 22:32:30 +0530

    added veiwattendance

--- src/Screen/Veiw_attendace/Veiwattendance.jsx
+++ src/Screen/Veiw_attendace/Veiwattendance.jsx
@@ -11,9 +11,37 @@ const Veiwattendance = () => {
   return (
     <div className='attendance-nav'>
        <Navbar toggleSidebar={toggleSidebar} />
+       <div className='dashboard-content'>
+       <div className={isNavbarOpen ? 'content-cover' : 'content-toggle'}>
+
       <div className='attendance'>
-           <h1>work in progress.....</h1> 
+      <span class="material-symbols-outlined  open" >
+double_arrow
+</span>
+           <h2 className='frnt'>View attendance Report</h2> 
+           
+        </div> 
+        <p className='line'>Admin can check attendance report of all users</p>
+      
+        <h3 className='srch'>Search by</h3>
+        <div className="by">
+        <button className='emp'>By Employee</button>
+        <button className='emp'>By Date</button>
         </div>
+        <h2 className='today'>Today's Statistics</h2>
+        <div className='rect'>
+           <div className='rect2'>
+            <p className='totl'>Total Number of Employee</p>
+             <h3 className='twenty'>20</h3>
+           </div>
+           <div className='rect3'>
+            <p className='totl'>Employee Present Today</p>
+            <h3 className='twenty'>0</h3>
+           </div>
+           
+        </div>
+         </div></div>
+       
     </div>
   )
 }
