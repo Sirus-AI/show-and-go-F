d8e911ffa3e3dd9adc455f46166dd7ae826569fc
Nehamalwal <nehaverma5017@gmail.in>
2024-06-24 18:27:05 +0530

    add file

--- src/Screen/Veiw_attendace/Veiwattendance.jsx
+++ src/Screen/Veiw_attendace/Veiwattendance.jsx
@@ -15,28 +15,38 @@ const Veiwattendance = () => {
        <div className={isNavbarOpen ? 'content-cover' : 'content-toggle'}>
 
       <div className='attendance'>
-      <span class="material-symbols-outlined  open" >
+      {/* <span class="material-symbols-outlined  open" >
 double_arrow
-</span>
-           <h2 className='frnt'>View attendance Report</h2> 
+</span> */}
+           {/* <h2 className='frnt'>View attendance Report</h2>  */}
            
         </div> 
-        <p className='line'>Admin can check attendance report of all users</p>
+        {/* <p className='line'>Admin can check attendance report of all users</p> */}
       
         <h3 className='srch'>Search by</h3>
-        <div className="by">
-        <button className='emp'>By Employee</button>
-        <button className='emp'>By Date</button>
+        <div className='by1'>
+           <select className='omp'>
+             <option className='op'>Select</option>
+             <option className='op'>By Employee</option>
+           </select>
+           <input className='omp1' type="Date" />
         </div>
+       
         <h2 className='today'>Today's Statistics</h2>
         <div className='rect'>
            <div className='rect2'>
             <p className='totl'>Total Number of Employee</p>
              <h3 className='twenty'>20</h3>
+             <span class="material-symbols-outlined , total1">
+person_add
+</span>
            </div>
            <div className='rect3'>
             <p className='totl'>Employee Present Today</p>
             <h3 className='twenty'>0</h3>
+            <span class="material-symbols-outlined , presnt">
+diversity_3
+</span>
            </div>
            
         </div>
--- src/Server.js
+++ src/Server.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const server = axios.create({
-    baseURL: 'http://127.0.0.1:8000/',
-    // baseURL: 'http://134.209.144.236:8000/',
+    // baseURL: 'http://127.0.0.1:8000/',
+    baseURL: 'http://16.171.144.129/',
 });
 
 server.interceptors.request.use((req) => {
