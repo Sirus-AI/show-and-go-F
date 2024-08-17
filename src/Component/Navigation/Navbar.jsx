ff5b7b218c6634f5cfdcf16ea6c5ba6d6c3ef4b7
Shivam-Acharya82 <your.shivamacharya82@gmail.com>
2024-08-17 15:47:52 +0530

    Dashboard ui change

--- src/Component/Navigation/Navbar.jsx
+++ src/Component/Navigation/Navbar.jsx
@@ -34,42 +34,47 @@ const Navbar = ({ toggleSidebar }) => {
 
     }, []);
     return (
-        <div className='nav-bar'>
-
-            <div className='navigation'>
-
-                <div className='nav-cover'>
-                    <div className='heading'>
-                        <span className='bar' onClick={handleToggleSidebar}>
-                            <span class="material-symbols-outlined">
-                                menu
-                            </span>
-                        </span>
-                        <div className='name'>
-                            {usertype === 1 ? (
-                                <span className='admin'>Super Admin Dashboard</span>
-                            ) : usertype === 2 ? (
-                                <span className='admin'>Admin Dashboard</span>
-                            ) : usertype === 3 ? (
-                                <span className='admin'>Organization Admin Dashboard</span>
-                            ) : usertype === 4 ? (
-                                <span className='admin'>User Dashboard</span>
-                            ):( <span className='admin'>Loading....</span>)}
-                            <p>Overall Report of institution</p>
-                        </div>
-
-                    </div>
-                    <div className='show-btn'>
-                        <button className='show-go-btn'>Show and Go</button>
-                    </div>
-                </div>
-
-            </div>
-
+        <div className='shiv'> 
             <div className={showSidebar ? 'side-off' : 'side-bar-nav '}>
                 <Sidebar  usertype={usertype} />
             </div>
+            <div className='nav-bar'>
 
+                <div className='navigation'>
+
+                    <div className='nav-cover'>
+                        <div className='heading'>
+                            <span className='bar' onClick={handleToggleSidebar}>
+                                <span class="material-symbols-outlined">
+                                    menu
+                                </span>
+                            </span>
+                            <div className='name'>
+                                {usertype === 1 ? (
+                                    <span className='admin'>Super Admin Dashboard</span>
+                                ) : usertype === 2 ? (
+                                    <span className='admin'>Admin Dashboard</span>
+                                ) : usertype === 3 ? (
+                                    <span className='admin'>Organization Admin Dashboard</span>
+                                ) : usertype === 4 ? (
+                                    <span className='admin'>User Dashboard</span>
+                                ) : (<span className='admin'>Loading....</span>)}
+                                <p className='shiv'><b>Overall Report of institution, welcome back !!..<span>"Admin Name"</span></b></p>
+                            </div>
+
+                        </div>
+                        <div className='show-btn'>
+                            <button className='show-go-btn'>Show and Go</button>
+                        </div>
+                    </div>
+
+                </div>
+
+                {/* <div className={showSidebar ? 'side-off' : 'side-bar-nav '}>
+                <Sidebar  usertype={usertype} />
+            </div> */}
+
+            </div>
         </div>
     )
 }
